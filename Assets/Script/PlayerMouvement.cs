02b2a577846e231fc6d43f3c222af68503152743
Avalario20 <104756549+Avalario20@users.noreply.github.com>
2022-05-11 08:34:24 +0100

    jumpUpdate1

--- Assets/Script/PlayerMouvement.cs
+++ Assets/Script/PlayerMouvement.cs
@@ -22,7 +22,15 @@ public class PlayerMouvement : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     private float horizontalmovement;
 
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position); //on crée la zone sur la quelle on detectera qu'il touchera le sol
+        horizontalmovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; //tant qu'on appuis sur les fleches directionnel
 
+        //on déplace le joueur
+        MovePlayer(horizontalmovement);
+
+    }
     void Update()
     {
         if (Input.GetButtonDown("Jump") && isGrounded) //si on appuis sur espace et que le perso touche le sol alors..
@@ -33,15 +41,6 @@ public class PlayerMouvement : MonoBehaviour
         animator.SetFloat("Speed", CharacterVelocity);
     }
 
-    void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position); //on crée la zone sur la quelle on detectera qu'il touchera le sol
-        horizontalmovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; //tant qu'on appuis sur les fleches directionnel
-
-        //on déplace le joueur
-        MovePlayer(horizontalmovement);
-
-    }
     void MovePlayer(float _horizontalmovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalmovement, rb.velocity.y); //on lui crée un mouvement horizontal
